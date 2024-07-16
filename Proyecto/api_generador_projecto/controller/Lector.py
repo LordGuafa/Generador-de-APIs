0e63e8066cc6c7c8b26ffdba4c087e0e2836a304
SalazarMalagon <andressalazar062858@gmail.com>
2024-07-15 22:05:44 -0500

    odenando2

--- Proyecto/api_generador_projecto/controller/Lector.py
+++
@@ -1,4 +0,0 @@
-import xml.etree.ElementTree as ET
-
-def leer_xml(filename):
-    return ET.parse(f"schemas/{filename}").getroot()
--- Proyecto/api_generador_projecto/controller/__init__.py
+++
@@ -1,4 +0,0 @@
-from controller.Lector import leer_xml
-from controller.Script_tablas import generate_script
-from controller.Crear_Docker import create_docker
-from controller.Crear_api import crear_api
